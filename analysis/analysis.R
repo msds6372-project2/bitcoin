b52cc3b990280149a86edcec39f782242aa59692
Jostein Straume <joebarrystraume@gmail.com>
2017-10-31 18:42:17 -0700

    Create analysis file, which includes ACF and PACF plots for various differences of original and transformed data.

---
+++ analysis/analysis.R
@@ -0,0 +1,61 @@
+# Load lubridate package to convert dates from factor to date class
+library(lubridate)
+library(reshape2)
+library(ggplot2)
+library(forecast)
+
+# File path of the data set
+path <- "/Users/Jostein/Grad School/SMU/6372/project2/bitcoin/data/bitcoin_price.csv"
+
+# Read in the CSV file of the data set
+bitcoin <- read.csv(path, header = TRUE)
+
+# Create new variable time via Lubridate, then order data set by ascending time
+bitcoin$Time <- mdy(bitcoin$Date)
+bitcoin <- bitcoin[order(bitcoin$Time),]
+
+# Snapshot of the data set
+head(bitcoin)
+dim(bitcoin)
+str(bitcoin)
+
+# Line plot of daily closing price of bitcoin
+plot(bitcoin$Close, type = "l", xlab = "Time", ylab = "Close Prices", main = "Daily Close Prices of Bitcoin")
+
+# Let's check the diagnostic plots
+fitClose <- lm(Close ~ Time, bitcoin)
+par(mfrow = c(3, 2))
+plot(fitClose, which = 1:6)
+
+
+# Transformation appears to be needed
+bitcoin$Log_Close <- log(bitcoin$Close)
+bitcoin$Sqrt_Close <- sqrt(bitcoin$Close)
+
+# How do we get to stationary?
+# First differences for original and transformed data sets
+diff1 <- diff(bitcoin$Close, lag = 1)
+logDiff1 <- diff(bitcoin$Log_Close, lag = 1)
+sqrtDiff1 <- diff(bitcoin$Sqrt_Close, lag = 1)
+
+# Plot first differences for original and transformed data sets
+dev.off()
+par(mfrow = c(3, 1))
+plot(diff1, type = "l", xlab = "Time", ylab = "Difference", main = "First Difference for Original Data")
+plot(logDiff1, type = "l", xlab = "Time", ylab = "Difference", main = "First Difference for Logged Data")
+plot(sqrtDiff1, type = "l", xlab = "Time", ylab = "Difference", main = "First Difference for Square-root Data")
+
+# Full and partial auto-correlation plots for first differences of square-root data
+par(mfrow = c(2, 1))
+acf(sqrtDiff1, main = "Auto-correlation Function of the First Differences")
+pacf(sqrtDiff1, main = "Partial Auto-correlation Function of the First Differences")
+
+# Create second differences for square-root data
+# Line plot and correlation plots for second differences
+sqrtDiff2 <- diff(sqrtDiff1, lag = 1)
+dev.off()
+par(mfrow = c(3, 1))
+plot(sqrtDiff2, type = "l", xlab = "Time", ylab = "Difference", main = "Second Difference for Square-root Data")
+acf(sqrtDiff2, main = "Auto-correlation Function of the Second Differences")
+pacf(sqrtDiff2, main = "Auto-correlation Function of the Second Differences")
+
--- analysis/exploratory_data_analysis.R
+++ analysis/exploratory_data_analysis.R
@@ -1,17 +1,26 @@
 # install.packages('lubridate')
 # Load lubridate package to convert dates from factor to date class
 library(lubridate)
+library(reshape2)
+library(ggplot2)
+library(forecast)
 
 # File path of the data set
 path <- "/Users/Jostein/Grad School/SMU/6372/project2/bitcoin/data/bitcoin_price.csv"
 
 # Read in the CSV file of the data set
-bitcoin <- read.csv(path, header=TRUE)
+bitcoin <- read.csv(path, header = TRUE)
 
 # Create new variable time via Lubridate, then order data set by ascending time
+detach(bitcoin)
 attach(bitcoin)
 bitcoin$Time <- mdy(bitcoin$Date)
-bitcoin <- bitcoin[order(Time),]
+bitcoin <- bitcoin[order(bitcoin$Time),]
+
+# Convert factor variables into integers
+# Source: https://stackoverflow.com/questions/4798343/convert-factor-to-integer
+#bitcoin$Volume <- as.numeric(levels(Volume))[Volume]
+#str(bitcoin)
 
 # Snapshot of the data set
 head(bitcoin)
@@ -56,9 +65,47 @@ summary(fitLogHigh)
 summary(fitLogLow)
 summary(fitLogClose)
 
+# Correlation matrix heatmap
+# Source: http://www.sthda.com/english/wiki/ggplot2-quick-correlation-matrix-heatmap-r-software-and-data-visualization
+# Source: https://stackoverflow.com/questions/3571909/calculate-correlation-cor-for-only-a-subset-of-columns
+cormat <- round(cor(bitcoin[sapply(bitcoin, is.numeric)]), 2)
+head(cormat)
+
+# Get Lower triangle of the correlation matrix
+get_lower_tri <- function(cormat) {
+  cormat[upper.tri(cormat)] <- NA
+  return(cormat)
+}
+
+# Get upper triangle of the correlation matrix
+get_upper_tri <- function(cormat) {
+  cormat[lower.tri(cormat)] <- NA
+  return(cormat)
+}
+
+# Put helper functions to use
+upper_tri <- get_upper_tri(cormat)
+upper_tri
+
+# Melt the correlation matrix
+melted_cormat <- melt(upper_tri, na.rm = TRUE)
+head(melted_cormat)
+
+# Build the heatmap
+ggplot(data = melted_cormat, aes(Var2, Var1, fill = value))+
+  geom_tile(color = "white")+
+  scale_fill_gradient2(low = "blue", high = "red", mid = "white", midpoint = 0, limit = c(-1,1), space = "Lab", name="Pearson\nCorrelation")+
+  theme_minimal()+
+  theme(axis.text.x = element_text(angle = 45, vjust = 1, size = 12, hjust = 1))+
+  coord_fixed()
+
+
 
 
 timeseries <- ts(data=bitcoin$Log_Close, start=c(2013, 118), frequency=365)
 plot(timeseries)
 
 plot(bitcoin$Open)
+
+arima_fit <- auto.arima(bitcoin$Close)
+plot(forecast(arima_fit))
