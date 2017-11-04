fee85722691f9a7fa5dbd616411800f62c3f5e7c
Jostein Straume <joebarrystraume@gmail.com>
2017-11-03 18:59:10 -0700

    Finished model selection.

--- analysis/analysis.R
+++ analysis/analysis.R
@@ -76,3 +76,65 @@ lines(density(logDiff1), lwd = 2)
 
 
 plot(residuals(arima_fit))
+
+
+# Arima Modeling
+# Source: https://stats.stackexchange.com/questions/207473/how-to-set-the-prediction-range-of-arima-model-in-r
+# Source: https://www.otexts.org/fpp/8/7
+myts <-ts(data = bitcoin$Log_Close, start = c(2013, 118), frequency = 365)
+model <- auto.arima(myts[1:1545])
+summary(model)
+# h parameter forecasts 75 days ahead
+fcst <- forecast(model, h = 75)
+plot(fcst, main = "Bitcoin Logged Closing Price Forecast", 
+     ylab = "Logged Closing Price", xlab = "Year")
+lines(fitted(fcst), col = "red")
+accuracy(f = fcst, x = bitcoin$Log_Close[1546:1620])
+
+# Forecast of Bitcoin Closing Pricing 
+myts <-ts(data=bitcoin$Log_Close, start=c(2013, 118), frequency=365)
+fs <- forecast(myts, h=75, level=c(80,95))
+plot(f, main = "Bitcoin Logged Closing Price Forecast", 
+     ylab="Logged Closing Prices", xlab="Year")
+lines(fitted(fs), col="red")
+
+# Arima Modeling
+library(forecast)
+model <- auto.arima(bitcoin$Log_Close[1:1545])
+fcst <- forecast(model, h=75)
+accuracy(f=fcst, x=bitcoin$Log_Close[1546:1620])
+
+# Use this for R markdown file
+myts <-ts(data = bitcoin$Log_Close, start = c(2013, 118), frequency = 365)
+arima_fit <- auto.arima(myts, ic = "aic", trace = TRUE)
+summary(arima_fit)
+forecast_arima_fit <- forecast(arima_fit, h = 75)
+plot(forecast_arima_fit, xlab = "Time", ylab = "Logged Closing Price")
+accuracy(f = forecast_arima_fit, x = myts[1546:1620])
+#lines(fitted(forecast_arima_fit), col="red")
+# plot(ts(data=bitcoin$Log_Close, start = c(2013, 118), frequency = 12))
+# lines(ts(forecast_arima_fit, start = c(2013, 118), frequency = 12), col = "red")
+
+model1 <- Arima(myts, c(0, 0, 0))
+model2 <- Arima(myts, c(0, 1, 0))
+model3 <- Arima(myts, c(1, 1, 0))
+model4 <- Arima(myts, c(2, 1, 0))
+model5 <- Arima(myts, c(2, 2, 0))
+
+line1 <- c("Arima(0, 0, 0)", model1$sigma2, model1$aic, model1$bic)
+line2 <- c("Arima(0, 1, 0)", model2$sigma2, model2$aic, model2$bic)
+line3 <- c("Arima(1, 1, 0)", model3$sigma2, model3$aic, model3$bic)
+line4 <- c("Arima(2, 1, 0)", model4$sigma2, model4$aic, model4$bic)
+line5 <- c("Arima(2, 2, 0)", model5$sigma2, model5$aic, model5$bic)
+
+# Source: http://www.cyclismo.org/tutorial/R/tables.html
+# modelTable <- matrix(c("Arima(0, 0, 0)", model1$sigma2, model1$aic, model1$bic, 
+#                        "Arima(0, 1, 0)", model2$sigma2, model2$aic, model2$bic,
+#                        "Arima(1, 1, 0)", model3$sigma2, model3$aic, model3$bic,
+#                        "Arima(2, 1, 0)", model4$sigma2, model4$aic, model4$bic,
+#                        "Arima(2, 2, 0)", model5$sigma2, model5$aic, model5$bic,
+#                        "Auto.Arima(0, 1, 0)", arima_fit$sigma2, arima_fit$aic, arima_fit$bic), ncol = 4, byrow = TRUE)
+modelTable <- matrix(c(line1, line2, line3, line4, line5, 
+                       "Auto.Arima(0, 1, 0)", arima_fit$sigma2, arima_fit$aic, arima_fit$bic), ncol = 4, byrow = TRUE)
+colnames(modelTable) <- c("Model", "Variance", "AIC", "SBC")
+modelTable
